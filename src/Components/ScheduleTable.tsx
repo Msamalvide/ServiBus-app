81fb6fe68393c3d6083814e3ba0ac0b930585fdf
MartinSamalvide <martin.samalvide@monetae.io>
2026-03-26 11:29:32 -0300

    Correccion3

--- src/Components/ScheduleTable.tsx
+++ src/Components/ScheduleTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import calendario from '../assets/calendario.svg'
-import reloj from '../assets/reloj.svg'
+import reloj from '../assets/Reloj.svg'
 import ubicacion from '../assets/Destino.svg'
 import { type Horario } from '../Data/horarios';
 
